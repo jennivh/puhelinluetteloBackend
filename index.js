44fa260385fba4e8f93f98e6e80598825d7e099c
Jenni Harakka <jvharakka@gmail.com>
2024-03-25 16:35:56 +0200

    commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-
+const Person = require('./models/person')
 const cors = require('cors')
 app.use(cors())
 app.use(express.json())
@@ -37,47 +38,100 @@ app.get('/', (require, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then( p => {
+        console.log(p)
+        response.json(p)
+    })
 })
 
 app.get('/info', (request, response) => {
     date = new Date()
-    response.send(`<p>Phonebook has info for ${persons.length} people</p> <p>${date}</p>`)
+    Person.find({}).then( p => {
+        response.send(`<p>Phonebook has info for ${p.length} people</p> <p>${date}</p>`)
+    })
+    
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(p => p.id !== id)
-    response.status(204).end()
-})
-
-app.post('/api/persons', (request, response) => {
+app.put('/api/persons/:id', (request, response, next) => {
     const body = request.body
-    const names = persons.map(p => p.name)
 
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, {new: true, runValidators: true, context: 'query'})
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+    const id = request.params.id
+    Person.findByIdAndDelete(id).then( result => {
+        response.json(result)
+        response.status(204).end()
+    })
+    .catch(error => {
+        next(error)
+    })
+})
+
+app.post('/api/persons', (request, response, next) => {
+    const body = request.body
+    
     if(!body.number || !body.name){
         return response.status(400).json({ error: "person must have name and number"})
     }
-    if (names.find(n => n === body.name)){
-        return response.status(400).json({error: "name must be unique"})
-    }
 
-    const person = {...request.body, id: Math.floor(Math.random() * (3000 - 1) + 1)}
-    response.json(person)
-    persons = persons.concat(person)
+    const person = new Person({
+        name: body.name,
+        number: body.number
+    })
+    
+    person.save()
+        .then(savedPerson => {
+        response.json(savedPerson)
+    })
+        .catch(error => next(error))
+
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    
-    const person = persons.find(p => p.id === id)
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
-    
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+
+// olemattomien osoitteiden käsittely
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.log(error.name)
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } else if(error.name === "ValidationError"){
+        return response.status(400).json({error: error.message})
+    }
+
+    next(error)
+  }
+
+ // tämä tulee kaikkien muiden middlewarejen ja routejen rekisteröinnin jälkeen!
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
---
+++ models/person.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+const url = process.env.REACT_APP_MONGODB_URI
+
+console.log('connecting to', url)
+mongoose.connect(url)
+.then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+
+const personSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      minlength: 3,
+      required: true,
+      unique: true
+    },
+    number: {
+      type: String,
+      minlength: 8,
+      validate: {
+        validator: v => {
+          return /\d{2,4}-\d+/.test(v) ;
+        },
+        message:'Phonenumber must start with 2 or 3 numbers'
+      },
+      required: true
+}})
+
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+  })
+
+module.exports = mongoose.model('Person', personSchema, 'persons')
